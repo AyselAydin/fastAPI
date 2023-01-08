ba960239a29f2a1c100249bfd3bc710f5be2fd03
AyselAydin <ayselaydin01@gmail.com>
2023-01-08 14:41:43 +0300

    HTTPException

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import Optional
 from pydantic import BaseModel
 
@@ -44,19 +44,22 @@ def get_item2(auther:str):
     for id in books:
         if books[id]["auther"] == auther:
             return books[id]
-    return {"Data": "The book you were searching was not found"}
+    raise HTTPException(status_code = 404, detail="The book you were searching was not found")
+    #return {"Data": "The book you were searching was not found"}
 
 @app.get("/get-by-auther_optional", summary = "Optional parameters")
 def get_item2(auther:Optional[str] = None):
     for id in books:
         if books[id]["auther"] == auther:
             return books[id]
-    return {"Data": "The book you were searching was not found"}
+    raise HTTPException(status_code = 404, detail="The book you were searching was not found")
+    #return {"Data": "The book you were searching was not found"}
 
 @app.post("/add-book/{book_id}")
 def add_book(book_id:int, item: Item):
     if book_id in books:
-        return {"Error": "Book ID already exists"}
+        #return {"Error": "Book ID already exists"}
+        raise HTTPException(status_code = 405, detail="Book ID already exists")
     books[book_id] = {"name" : item.name, "price" : item.price, "genre" : item.genre, "auther" : item.auther}
     #books[book_id] = item
     return books[book_id]
@@ -64,7 +67,8 @@ def add_book(book_id:int, item: Item):
 @app.put("/update-book/{book_id}")
 def update_book(book_id:int, item: UpdateItem):
     if book_id not in books:
-        return {"Error": "Book Id doesn't exists"}
+        #return {"Error": "Book Id doesn't exists"}
+        raise HTTPException(status_code = 404, detail="Book Id doesn't exists")
 
     if item.name != None:
         books[book_id].name = item.name
@@ -80,7 +84,8 @@ def update_book(book_id:int, item: UpdateItem):
 @app.delete("/delete-book")
 def delete_book(book_id:int):
     if book_id not in books:
-        return {"Error":"book does not exists."}
+        #return {"Error":"Book Id doesn't exists"}
+        raise HTTPException(status_code = 404, detail="Book Id doesn't exists")
 
     del books[book_id]
     return {"Result": "The Book deleted"}
